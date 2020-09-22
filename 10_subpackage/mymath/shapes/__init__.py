8365bcbd6d98d5933b19f74ddd10db42b899023c
longTimeNoSee <johnberboy@naver.com>
2020-09-22 11:11:40 +0900

    Relative path

--- 10_subpackage/mymath/shapes/__init__.py
+++ 10_subpackage/mymath/shapes/__init__.py
@@ -1,5 +1,10 @@
-from mymath.shapes import area, volume
+# from mymath.shapes import area, volume
 # mymath : 패키지
 # shapes : 서브패키지
 # area : 모듈
 # volume : 모듈
+
+##################상대경로
+
+from .area import *
+from .volume import *
--- 10_subpackage/mymath/shapes/area.py
+++ 10_subpackage/mymath/shapes/area.py
@@ -1,6 +1,7 @@
 __all__ = ['PI', 'circle'] # from ~~.area import *
 # 서클을 실행할 때만 실행되는 특수변수나 함수.
 
+
 #from shapes import PI
 PI = 3.14 # 상수는 대문자로 지정
 
@@ -9,3 +10,8 @@ def circle(radius):
 
 def square(length):
     return length * length
+
+
+# 상대경로
+from ..stats.average import data_mean
+# 복잡해보이는 경우 절대경로가 더욱 직관적
